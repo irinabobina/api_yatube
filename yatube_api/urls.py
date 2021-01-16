0ca47d9c8691b5cf1126c0fa76f05b6757394338
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-16 08:37:27 +0300

    kjnk

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('posts.urls')),
+    path('', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
