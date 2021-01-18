35aeea5191cdc4923afd95a0279006c019d46cb5
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-18 21:38:34 +0300

    jhvj

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from posts.models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source="author.username", read_only=True)
+    author = serializers.StringRelatedField(source="author.username", read_only=True)
 
     class Meta:
         fields = '__all__'
@@ -11,7 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source="author.username", read_only=True)
+    author = serializers.StringRelatedField(source="author.username", read_only=True)
 
     class Meta:
         fields = '__all__'
