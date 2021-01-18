b5fdb71de8aa48a30aa24db5752ddc9feaaad1c6
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-19 00:01:45 +0300

    jhgvjh

--- api/views.py
+++ api/views.py
@@ -28,13 +28,7 @@ class APICommentViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnlyPermission] 
      
     def perform_create(self, serializer): 
-        serializer.save(author=self.request.user) 
-
-    #def list(self, request, post_pk):
-        #if Comment.objects.filter(post=post_pk).exists():
-        #comments = Comment.objects.filter(post=post_pk) 
-        #serializer = CommentSerializer(comments, many=True) 
-        #return Response(serializer.data)
+        serializer.save(author=self.request.user)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id = self.kwargs.get('post_pk'))
