89743092c9297228577bbdc8650a70e461bf35d6
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-18 22:38:06 +0300

    kjlnl

--- api/urls.py
+++ api/urls.py
@@ -12,11 +12,7 @@ router.register('posts', APIPostViewSet)
 router.register(r'posts/(?P<post_pk>\d+)/comments', APICommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('v1/', include(router.urls))
-]
-
-urlpatterns = [
-    #path('v1/', include(router.urls)),
+    path('v1/', include(router.urls)),
     path('', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),  
 ]
