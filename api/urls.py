9879727603eebde49cf7614e1cf46ad343a565cd
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-18 23:13:56 +0300

    лило

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path
+from django.urls import include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
