c0d152215ad17dce39283706dd2b036cb8232fb5
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-19 00:15:04 +0300

    л б

--- api/views.py
+++ api/views.py
@@ -1,35 +1,34 @@
 from rest_framework import permissions
-from rest_framework import viewsets 
-from rest_framework import status 
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from django.shortcuts import get_object_or_404
 
 from posts.models import Post, Comment
 from api.serializers import PostSerializer, CommentSerializer
 from api.permissions import IsAuthorOrReadOnlyPermission
- 
- 
-class APIPostViewSet(viewsets.ModelViewSet): 
-    queryset = Post.objects.all() 
-    serializer_class = PostSerializer 
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnlyPermission] 
-
-    def perform_create(self, serializer): 
-        serializer.save(author=self.request.user) 
-
-    def perform_update(self, serializer): 
-        serializer.save() 
 
 
-class APICommentViewSet(viewsets.ModelViewSet): 
-    queryset = Comment.objects.all() 
-    serializer_class = CommentSerializer 
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnlyPermission] 
-     
-    def perform_create(self, serializer): 
+class APIPostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnlyPermission]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class APICommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnlyPermission]
+
+    def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id = self.kwargs.get('post_pk'))
         return post.comments
+
