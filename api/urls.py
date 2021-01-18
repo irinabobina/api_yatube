c59c999d03dabc7c0d912a26f428a62e0f7c38e2
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-18 22:06:15 +0300

    gfbg

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,6 @@ router.register('posts', APIPostViewSet)
 router.register(r'posts/(?P<post_pk>\d+)/comments', APICommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
