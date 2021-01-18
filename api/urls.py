e01ad5fec21826cfc30b9e740b3a079c0b15c420
Ирина Бобина <bobin.ir495@gmail.com>
2021-01-18 22:21:10 +0300

    kjjn

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,8 @@ router = DefaultRouter()
 router.register('posts', APIPostViewSet)
 router.register(r'posts/(?P<post_pk>\d+)/comments', APICommentViewSet, basename='comments')
 
-urlpatterns = [ 
+urlpatterns = [
+    path('v1/', include(router.urls)),
     path('', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
-    path('v1/', include(router.urls)),  
-] 
+    path('api-token-auth/', views.obtain_auth_token),  
+]
